923a6d8bdfcfc2b56e231399c8f4ca73d4bce329
Won Hyoung <163235014+eFOROW@users.noreply.github.com>
2025-05-31 15:39:41 +0900

    [Chore] 로그인 페이지 샘플 UI 구현 (#5)
    * feat: 샘플 UI 개발
    
    * feat: /register Page Redirect

---
+++ src/app/(pages)/register/page.tsx
@@ -0,0 +1,14 @@
+import Image from 'next/image';
+
+export default function register() {
+  return (
+    <div className="flex flex-col min-h-[80vh] justify-between items-center overflow-hidden" style={{ fontFamily: 'Noto Sans KR, sans-serif' }}>
+      <main className="w-full max-w-[90%] md:max-w-[70%] lg:max-w-[50%] flex-grow flex items-center justify-center">
+        <div className="flex flex-col items-center">
+            {/* contents */}
+            register
+        </div>
+      </main>
+    </div>
+  );
+}
