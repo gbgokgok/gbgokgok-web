689a8e1c18d6a9d8d0705caaa5ffe42a25734fc0
Won Hyoung <163235014+eFOROW@users.noreply.github.com>
2025-07-26 17:06:05 +0900

    fix. : standalone

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'standalone',
   env: {
     IS_LOCAL_ENV: process.env.NODE_ENV === 'development' ? 'true' : 'false',
   },
